7d0c19adf21d32def6cffaa8ad3b04405e211718
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2025-10-30 16:23:44 -0500

    feat se crea inicio de la pp configurando mensaje de inicio con colores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,58 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Comienza aca: ");
+        //System.out.printf("Comienza aca: ");
+
+        //Entradas de algoritmo1
+        Scanner Sc = new Scanner(System.in);
+        String colorNegro = "\u001B[30m";
+        String colorRojo = "\u001B[31m";
+        String colorVerde = "\u001B[32m";
+        String colorAmarillo = "\u001B[33m";
+        String colorAzul = "\u001B[34m";
+        String colorMorado = "\u001B[35m";
+        String colorBlanco = "\u001B[29m";
+
+        String correoDigitaUsuario;
+        String contraseñaDigitaUsuario;
+        String correoGuardadoBaseDatos = "cr@gmail.com";
+        String ContraseñaGuardadaBaseDatos = "Cr7teamo"
 
 
+        //Procesos
+        System.out.println(colorV+"************************");
+        System.out.println("GESTOR APP BY JJGR 2025 🦒🦒🦒");
+        System.out.println("************************");
+
+
+
+        //diseñar un algoritmo que permita el ingreso
+        //de un usuario con correo y contraseña
+        //se permitiran 3 intentos antes de bloquearlos
+
+        //diseñar un algoritmo que permita mostrar un menu de opcione
+        //si el usuario se autentico con exito el menu sera el siguiente
+        //1. Registrar venta
+        //2. Mostrar ventas del dia
+        //3. Salir
+
+        //NOTA: Una vena se considera en el sistea como una variable
+        //con los siguientes datos:
+        //id--> entero
+        //fecha-->LocalDate
+        //descipcion --> Cadena
+        //Lista de produtos --> Lista
+        //Total --> double
+
+        //NOTA2: un producto tiene:
+        //id
+        //preciounitario
+        //fotografia
+        //cantidad
     }
 }
